a6b362c0134d664ce223366efc3a7167d609b234
John Tom Jr <johntjr.22@gmail.com>
2020-03-22 05:45:47 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,6 +7,6 @@ if user_input == "exit"
   print "Goodbye!"
 elsif 
 user_input !=random_number
-print "Sorry! The computer guessed #{random_number}"
+print "Sorry! The computer guessed#{random_number}"
 end
 end
