2107325cec1c3b1e29ed2b61793832e2440ce84b
John Tom Jr <johntjr.22@gmail.com>
2020-03-22 05:44:11 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,5 +3,10 @@ def run_guessing_game
 random_number = rand(1..6)
 puts "Pick a number between 1 and 6"
 user_input = gets.chomp
-
+if user_input == "exit"
+  print "Goodbye!"
+elsif 
+user_input !=random_number
+print "Sorry! The computer guessed #{random_number}"
+end
 end
